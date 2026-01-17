85a3579651305aacc01d3e0128673daefa28832b
Mouaz-Ahmed-Alazazy <mouazmaama@gmail.com>
2026-01-17 22:55:36 +0200

    edit queue/urls.py

--- queues/urls.py
+++ queues/urls.py
@@ -2,7 +2,6 @@
 URL configuration for queues app.
 """
 from django.urls import path
-from . import views
 from .views import QRScannerView, ProcessCheckInView, PatientQueueStatusView
 
 app_name = 'queues'
