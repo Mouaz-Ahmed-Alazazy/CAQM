391ec79dc8df64ae71575b1233ee47dbc5f9e295
Mouaz-Ahmed-Alazazy <mouazmaama@gmail.com>
2026-01-18 00:53:00 +0200

    update queues/urls.py again.

--- queues/urls.py
+++ queues/urls.py
@@ -2,7 +2,7 @@
 URL configuration for queues app.
 """
 from django.urls import path
-from .views import QRScannerView, ProcessCheckInView, PatientQueueStatusView
+from .views import QRScannerView, ProcessCheckInView, PatientQueueStatusView, CallNextPatientView
 
 app_name = 'queues'
 
@@ -10,5 +10,5 @@ urlpatterns = [
     path('scan/', QRScannerView.as_view(), name='qr_scanner'),
     path('checkin/', ProcessCheckInView.as_view(), name='process_checkin'),
     path('status/', PatientQueueStatusView.as_view(), name='queue_status'),
-    path('call-next/', views.CallNextPatientView.as_view(), name='call_next_patient'),
+    path('call-next/', CallNextPatientView.as_view(), name='call_next_patient'),
 ]
