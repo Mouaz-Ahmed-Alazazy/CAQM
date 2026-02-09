2c48f72cdd30a4b00c0b4d58fb191485ef44916e
Mouaz-Ahmed-Alazazy <mouazmaama@gmail.com>
2026-02-09 13:01:45 +0200

    about ending the google authentication

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PatientRegistrationView, CustomLoginView, CustomLogoutView
+from .views import PatientRegistrationView, CustomLoginView, CustomLogoutView, ProfileUpdateView
 
 app_name = 'accounts'
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('register/', PatientRegistrationView.as_view(), name='patient_register'),
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', CustomLogoutView.as_view(next_page='accounts:login'), name='logout'),
+    path('profile/update', ProfileUpdateView.as_view(), name='profile_update'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 from django.urls import reverse_lazy
 from django.contrib import messages
 from django.shortcuts import redirect
@@ -187,3 +187,37 @@ class CustomLogoutView(LogoutView):
         if request.user.is_authenticated:
             messages.success(request, 'You have been logged out successfully.')
         return super().dispatch(request, *args, **kwargs)
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'phone', 'date_of_birth', 'gender']
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'phone': forms.TextInput(attrs={
+                'class': 'form-control',
+                'pattern': '(091|092|093|094)[0-9]{7}',
+                'placeholder': '0911234567'
+            }),
+            'date_of_birth': forms.DateInput(attrs={
+                'class': 'form-control',
+                'type': 'date'
+            }),
+            'gender': forms.Select(attrs={'class': 'form-control'}),
+        }
+
+
+class ProfileUpdateView(LoginRequiredMixin, UpdateView):
+    model = User
+    form_class = ProfileUpdateForm
+    template_name = 'accounts/profile_update.html'
+    success_url = reverse_lazy('patients:my_appointments')
+    
+    def get_object(self):
+        return self.request.user
+    
+    def form_valid(self, form):
+        messages.success(self.request, 'Profile updated successfully!')
+        return super().form_valid(form)
